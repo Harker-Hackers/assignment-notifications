ba45608d0ae8222aa5e42e90629a2f18702c6dde
goombamaui <77907708+goombamaui@users.noreply.github.com>
2021-01-24 15:08:12 -0800

    Update __init__.py
    Fix for localhost update: added redir

--- schoolopyInfo/__init__.py
+++ schoolopyInfo/__init__.py
@@ -1,5 +1,13 @@
 import schoolopy
 from os import getenv
+from flask import url_for, request
+from app import app
 schoolopyAuth = schoolopy.Auth(getenv('SCHOOLOGY_KEY'), getenv('SCHOOLOGY_SECRET'), three_legged=True, domain='https://schoology.harker.org/')
+
+#url for auth
+authUrl="/authorized"
+
+#faking a request to get host
 with app.test_request_context("/"):
-    schoolopyUrl = schoolopyAuth.request_authorization().replace('https%3A%2F%2Fschoology.harker.org%2F', request.host+"/authorized")
+    schoolopyUrl = schoolopyAuth.request_authorization().replace('https%3A%2F%2Fschoology.harker.org%2F', request.host+authUrl)
+    
