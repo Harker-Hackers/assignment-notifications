cf345f7c1b56d992ef14b9f3f4d1faa502bba22e
goombamaui <77907708+goombamaui@users.noreply.github.com>
2021-01-24 15:07:02 -0800

    Update __init__.py
    I have fixed the random localhost redirect

--- schoolopyInfo/__init__.py
+++ schoolopyInfo/__init__.py
@@ -1,4 +1,5 @@
 import schoolopy
 from os import getenv
 schoolopyAuth = schoolopy.Auth(getenv('SCHOOLOGY_KEY'), getenv('SCHOOLOGY_SECRET'), three_legged=True, domain='https://schoology.harker.org/')
-schoolopyUrl = schoolopyAuth.request_authorization().replace('https%3A%2F%2Fschoology.harker.org%2F', getenv('HOME_URL') + '/authorized')
+with app.test_request_context("/"):
+    schoolopyUrl = schoolopyAuth.request_authorization().replace('https%3A%2F%2Fschoology.harker.org%2F', request.host+"/authorized")
