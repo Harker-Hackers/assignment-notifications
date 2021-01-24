f8be7b5a19266ccbb4233879633c262d5375ec88
goombamaui <77907708+goombamaui@users.noreply.github.com>
2021-01-24 15:26:20 -0800

    Update __init__.py
    /authorized, not /authorize

--- schoolopyInfo/__init__.py
+++ schoolopyInfo/__init__.py
@@ -4,4 +4,4 @@ from flask import url_for, request
 from app import app
 schoolopyAuth = schoolopy.Auth(getenv('SCHOOLOGY_KEY'), getenv('SCHOOLOGY_SECRET'), three_legged=True, domain='https://schoology.harker.org/')
 host=getenv("HOME_URL")
-schoolopyUrl = schoolopyAuth.request_authorization().replace('https%3A%2F%2Fschoology.harker.org%2F', host+"/authorize")
+schoolopyUrl = schoolopyAuth.request_authorization().replace('https%3A%2F%2Fschoology.harker.org%2F', host+"/authorized")
