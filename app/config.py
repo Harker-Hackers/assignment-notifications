734444e2a75e23c45b3e72865b5fa6b257b0a518
unknown <25aarushv@students.harker.org>
2021-01-25 08:13:51 -0800

    fixing requirements.txt

--- app/config.py
+++ app/config.py
@@ -11,4 +11,4 @@ class Config(object):
     MAIL_USE_SSL = False
     #database
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir, 'app.db')
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
